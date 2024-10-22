d051abbb32885839a11b2453bef4ecb681db1cac
elefteron <elefter@abv.bg>
2024-10-22 17:11:25 +0300

    kill js
    kill js

--- fontorg/fnt.reg/How To Export the Fonts Section from the Windows Registry to a text file_files/sticky.js
+++
@@ -1,1 +0,0 @@
-function adjustSidebar(){if($(".sticky").offset()){var s=$(".sticky").offset().top;$(window).scroll(function(){var i=$(window).scrollTop(),t=$(".sticky").width();s<(i+=50)?($(".sticky").css({position:"fixed",top:50}),$(".sticky").width(t)):($(".sticky").css("position","static"),$(".sticky").width("auto"))})}}
